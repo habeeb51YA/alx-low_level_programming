6d8a99b0ac087d05fda156afa414b81dd45aa136
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-13 17:16:57 -0700

    stash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+\**
+* main - Entry Point
+*
+* Return: Always 0 (sucess)
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
