f97b7404c4b841215079bd0a81959524efb82a53
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-15 14:10:09 -0700

    d

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+char letter = 'a';
+int count = 0;
+
+while (count < 10)
+{
+char currentLetter = 'a';
+while (currentLetter <= 'z')
+{
+putchar(currentLetter);
+currentLetter++;
+}
+putchar('\n');
+count++;
+}
+}
+
+/**
+ * main - prints alphabets called
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+printAlphabetTenTimes();
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,4 @@
 /*function prototype for _putchar*/
 int main(void);
 void print_alphabet(void);
+void print_alphabet_x10(void);
