94bb00d9140a694734defdf7f9457a137db8ccf9
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-16 08:54:08 -0700

    dd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,14 +8,6 @@
  */
 int main(void)
 {
-putchar('');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+printf("_putchar\n");
 return (0);
-}
+
