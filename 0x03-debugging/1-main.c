f5e55ce41d9b93fb1f4a5b41f17da8ce00e894b2
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-17 10:41:03 -0700

    stash

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
-
 int main(void)
 {
         int i;
@@ -13,10 +6,12 @@ int main(void)
 
         i = 0;
 
-        /while (i < 10)/
-        /|/
-        /*      putchar(i);*/
-        /|/
+        /**
+	 * while (i < 1)
+	 * {
+         * putchar(i);
+	 * }
+	 */
 
         printf("Infinite loop avoided! \\o/\n");
 
