a2bb46091ed5d2202a6f6435a21cab8193459066
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-19 11:52:13 -0700

    g

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply the code
+ * @num1: integer 1
+ * @num2: integer 2
+ * Return: Always 0.
+ */
+
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
