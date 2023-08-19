32a470aad7dd63a750119e35f3bfc8741ebc1d58
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-19 12:04:05 -0700

    g

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prinnt numbers 0 - 9
+ * @num1: interger 1
+ * @num2: interger 2
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(int num1, int num2)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
