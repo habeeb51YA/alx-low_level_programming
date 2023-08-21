20570ba5007719c1f64fa22d69b0e89437bb4e17
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-21 12:12:25 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,33 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
- * _strlen - Calculates the length of a string.
- * @s: Pointer to the string.
- *
- * Return: The length of the string.
- */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-
-	return (length);
-}
-
-/**
- * print_rev - Prints a string in reverse followed by a new line.
+ * print_reverse - Prints a string in reverse followed by a new line.
  * @s: Pointer to the string.
  */
-void print_rev(char *s)
+
+void print_reverse(char *s)
 {
-	int length = _strlen(s);
+	int length = strlen(s);
 
 	for (int i = length - 1; i >= 0; i--)
 	{
