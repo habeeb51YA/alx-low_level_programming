e60e967e9dfd800d8b921a93bf642f2da52d0171
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-21 12:07:10 -0700

    gggg

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_string - Reverses a string in place.
+ * @s: Pointer to the string.
+ */
+
+void reverse_string(char *s)
+{
+	int length = strlen(s);
+	int start = 0;
+	int end = length - 1;
+
+	while (start < end)
+	{
+		char temp = s[start];
+
+		s[start] = s[end];
+
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
