f254465b1778792527711202c1fb3a4fbb6f725a
habeeb51YA <habibu.yusuf@pramie.tech>
2023-08-22 18:17:08 -0700

    g

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,39 +11,38 @@
 int _atoi(char *s)
 {
 	int result = 0;
+
 	int sign = 1;
-	int i = 0;
 
-	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+	int digit;
+
+	while (*s)
 	{
-		i++;
-	}
-
-	if (s[i] == '-' || s[i] == '+')
-	{
-		sign = (s[i] == '-') ? -1 : 1;
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-
-		if (result > (2147483647 - (s[i] - '0')) / 10)
+		if (*s == '-')
 		{
+			sign = -sign;
+		}
+		else if (*s >= '0' && *s <= '9')
+			{
+			digit = *s - '0';
 
-			if (sign == 1)
+			if (sign == 1 && (result > (2147483647 - digit) / 10))
 			{
 				return (2147483647);
 			}
-			else
+			else if (sign == -1 && (result < (-2147483648 + digit) / 10))
 			{
 				return (-2147483648);
 			}
+			result = result * 10 + (sign * digit);
+		}
+		else if (result != 0)
+		{
+			break;
 		}
 
-		result = result * 10 + (s[i] - '0');
-		i++;
+		s++;
 	}
 
-	return (result * sign);
+	return (result);
 }
