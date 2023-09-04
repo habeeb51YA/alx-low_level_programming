ed2a8c8fbf372ab63f10fcdf0b26e51711380291
habeeb51YA <habibu.yusuf@pramie.tech>
2023-09-04 16:29:19 -0700

    001

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,20 +13,16 @@
 char *_strdup(char *str)
 {
 	char *duplicate;
-	unsigned int len, i;
 
 	if (str == NULL)
 		return (NULL);
 
-	for (len = 0; str[len] != '\0'; len++)
-
-	duplicate = malloc(sizeof(char) * (len + 1));
+	duplicate = (char *)malloc(strlen(str) + 1);
 
 	if (duplicate == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len; i++)
-		duplicate[i] = str[i];
+	strcpy(duplicate, str);
 
 	return (duplicate);
 }
