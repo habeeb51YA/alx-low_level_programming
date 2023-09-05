0810f96324239501b107367a887e2ba0e4172426
habeeb51YA <habibu.yusuf@pramie.tech>
2023-09-04 17:33:33 -0700

    21

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2-dimensional grid.
+ * @grid: Pointer to the grid.
+ * @height: Height of the grid.
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (int i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
