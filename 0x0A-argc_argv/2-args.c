bd8e137746d17c591cfc0991a9ae78b4e73aacf4
habeeb51YA <habibu.yusuf@pramie.tech>
2023-09-06 17:49:04 -0700

    02

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,15 +3,19 @@
 /**
  * main - Entry point of the program.
  * @argc: The number of arguments passed to the program.
- * @argv: An array of strings containing the program's arguments.
+ * @argv: An array of strings.
  *
  * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	void()argv;
+	int i;
 
-	printf("%d\n", argc - 1);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
 	return (0);
 }
