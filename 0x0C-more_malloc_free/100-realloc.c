0663747ee0fa2591352917ff8e855ee96c8e1b4f
habeeb51YA <habibu.yusuf@pramie.tech>
2023-09-08 13:23:50 -0700

    101..

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -41,7 +41,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size < old_size)
 		old_size = new_size;
 
-	for (int i = 0; i < old_size; i++)
+	for (i = 0; i < old_size; i++)
 		((char *)new_ptr)[i] = ((char *)ptr)[i];
 
 	free(ptr);
