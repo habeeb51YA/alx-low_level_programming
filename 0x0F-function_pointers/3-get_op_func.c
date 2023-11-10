5c0e19a43562b6f1950ab96092d9de43d9c36371
habeeb51YA <habibu.yusuf@pramie.tech>
2023-11-10 10:32:36 -0800

    func

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include "stddef.h"
+
+/*
+ * get_op_function - get op function pointer with type char
+ *
+ * @s: a charater pointer to a symbol
+ *
+ * Return: one of the operator funtions
+ */
+
+int (*get_op_func(char *s))(int, int);
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{“ -“, op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * op_add - adds two values @a and @b
+ *
+ * @a: value a
+ * @b: value b
+ *
+ * Return: sum of @a and @b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - minus two values @a and @b
+ *
+ * @a: value a
+ * @b: value b
+ *
+ * Return: reminder of @a and @b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two values @a and @b
+ *
+ * @a: value a
+ * @b: value b
+ *
+ * Return: multiplication of @a and @b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two values @a and @b
+ *
+ * @a: value a
+ * @b: value b
+ *
+ * Return: division of @a and @b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - reminder of two values @a and @b
+ *
+ * @a: value a
+ * @b: value b
+ *
+ * Return: remainder of @a and @b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
