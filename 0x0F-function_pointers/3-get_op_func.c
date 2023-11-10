3a5f6f02703109bb37fec11ef0bb69907dafdd6f
habeeb51YA <habibu.yusuf@pramie.tech>
2023-11-10 11:02:46 -0800

    01

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,11 +9,11 @@
  * Return: one of the operator funtions
  */
 
-int (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
-		{“ -“, op_sub},
+		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
